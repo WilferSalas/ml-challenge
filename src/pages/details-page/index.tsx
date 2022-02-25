13d358e751ce23a1d0de091b8a2890565f559357
Wilfer Salas <sony634@hotmail.com>
2022-02-24 22:24:09 -0500

    add breadcrumbs to details page

--- src/pages/details-page/index.tsx
+++ src/pages/details-page/index.tsx
@@ -7,8 +7,10 @@ import Paper from "@mui/material/Paper";
 import React from "react";
 import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom";
+import { useQueryClient } from "react-query";
 
 // @scripts
+import BreadcrumbsComponent from "../../components/breadcrumbs-component";
 import ProgressComponent from "../../components/progress-component";
 import ProductNotFound from "./ProductNotFound";
 import { useFetchProduct } from "../../api";
@@ -17,8 +19,13 @@ import { useFetchProduct } from "../../api";
 import { Product } from "../../interfaces";
 
 const DetailsPage = () => {
+  const queryClient = useQueryClient();
   const { id } = useParams();
   const { data, isLoading } = useFetchProduct(id);
+
+  const cachedData: any = queryClient.getQueryData(["products"], {
+    exact: false,
+  });
 
   if (isLoading)
     return <ProgressComponent id="details-page" isFullPage size={50} />;
@@ -27,7 +34,7 @@ const DetailsPage = () => {
 
   const formatter = new Intl.NumberFormat("sfb", {
     style: "currency",
-    currency: data.item.price.currency,
+    currency: data?.item?.price.currency || "USD",
     currencyDisplay: "narrowSymbol",
   });
 
@@ -39,41 +46,45 @@ const DetailsPage = () => {
     price: { amount },
     sold_quantity,
     title,
-  }: Product = data.item;
+  }: Product = data.item || {};
 
   return (
-    <Paper component={Container} id="home-page" sx={{ mt: 5 }}>
-      <Grid container sx={{ padding: 5 }}>
-        <Grid item xs={7} justifyContent="center" sx={{ display: "flex" }}>
-          <Box
-            component="img"
-            alt={idProduct}
-            src={picture}
-            sx={{ maxWidth: 400 }}
-          />
+    <Container>
+      <BreadcrumbsComponent id="details-page" items={cachedData?.categories} />
+      <Paper id="home-page">
+        <Grid container sx={{ padding: 5 }}>
+          <Grid item xs={7} justifyContent="center" sx={{ display: "flex" }}>
+            <Box
+              component="img"
+              alt={idProduct}
+              src={picture}
+              sx={{ maxWidth: 400 }}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="subtitle1">
+              {condition === "new" ? "Nuevo" : "Usado"} - {sold_quantity}
+              vendidos
+            </Typography>
+            <Typography gutterBottom variant="h5" sx={{ fontWeight: 600 }}>
+              {title}
+            </Typography>
+            <Typography gutterBottom variant="h4" sx={{ mb: 5 }}>
+              {formatter.format(amount)}
+            </Typography>
+            <Button color="secondary" fullWidth variant="contained">
+              Comprar
+            </Button>
+          </Grid>
+          <Grid item sx={{ mt: 8 }} xs={12}>
+            <Typography gutterBottom variant="h5">
+              Descripcion del producto
+            </Typography>
+            <Typography>{description}</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <Typography variant="subtitle1">
-            {condition === "new" ? "Nuevo" : "Usado"} - {sold_quantity} vendidos
-          </Typography>
-          <Typography gutterBottom variant="h5" sx={{ fontWeight: 600 }}>
-            {title}
-          </Typography>
-          <Typography gutterBottom variant="h4" sx={{ mb: 5 }}>
-            {formatter.format(amount)}
-          </Typography>
-          <Button color="secondary" fullWidth variant="contained">
-            Comprar
-          </Button>
-        </Grid>
-        <Grid item sx={{ mt: 8 }} xs={12}>
-          <Typography gutterBottom variant="h5">
-            Descripcion del producto
-          </Typography>
-          <Typography>{description}</Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Container>
   );
 };
 
